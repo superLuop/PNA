aadfbea30bb81fcd0f39815cb99f5241c79b9d08
superLuop <PLuo2018@163.com>
2019-12-19 19:03:10 +0800

    update

--- src/org/xidian/ui/Background.java
+++ src/org/xidian/ui/Background.java
@@ -23,7 +23,7 @@ public class Background extends JPanel {
 		setBounds(0, 0, (int) screenSize.getWidth(),
 				(int) screenSize.getHeight());		
 		g.drawImage(new ImageIcon(this.getClass().getResource(
-				"/images/background.jpg")).getImage(), 0, 0, getWidth(), getHeight(), this);
+				"/images/background.png")).getImage(), 0, 0, getWidth(), getHeight(), this);
 		MainFrame.getInstance().repaint();
 	}
 	
